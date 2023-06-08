b594804a349cd52eee37ad39436d165da9be50b3
canyapalak <canyapalak@gmail.com>
2023-06-08 16:44:29 +0200

    ✨ created navbar

---
+++ src/data/navData.ts
@@ -0,0 +1,34 @@
+import { navDataArray } from "@/types/navDataType";
+
+export const navData: navDataArray = [
+    {
+    id: 1,
+    text: "Home",
+    path: "/"
+},
+    {
+        id: 2,
+            text: "Language",
+    path: ""
+    },
+    {
+        id: 3,
+    text: "Theme",
+    path: ""
+    },
+    {
+        id: 4,
+    text: "Wiki",
+    path: "/wiki"
+    },
+    {
+        id: 5,
+    text: "Login",
+    path: "/login"
+    },
+    {
+        id: 6,
+    text: "Profile",
+    path: "/profile"
+    }
+]
