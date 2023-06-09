772e3ae725ecd49e53c19c46cde8a35e6a9e5d0a
santiagomanso <santiagomanso@web.de>
2023-06-09 18:50:52 +0200

    🎨 reworked homePage intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Container from '@/components/containers/Container'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
   return (
@@ -10,16 +12,30 @@ export default function Home() {
           className='w-full h-full object-cover'
         />
       </article>
-      <article className='flex flex-col px-10 items-center justify-center w-1/2'>
+      <article className='flex flex-col gap-3 px-10 items-center justify-center w-1/2'>
         <h1 className='text-4xl tracking-wider text-gray-700 font-play'>
-          Welcome to RPG
+          Welcome to Oblivion
         </h1>
         <p className='text-gray-600 font-play'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-          veritatis commodi consequatur provident neque explicabo. Rem corporis,
-          ipsa ipsam incidunt et ea sapiente voluptates quod, illum ab
-          aspernatur perferendis ducimus?
+          Embark on a perilous quest with a singular goal: to vanquish the
+          indomitable Infernus, Lord of Oblivion. Brace yourself for an epic
+          battle against the embodiment of darkness, where the fate of all
+          realms hangs in the balance. Explore treacherous landscapes, unravel
+          the mysteries that shroud Infernus origins, and face relentless
+          challenges that test your mettle. Rally your allies, sharpen your
+          skills, and summon unwavering courage as you traverse the depths of
+          this malevolent realm. Your journey into the heart of darkness is the
+          key to saving existence itself. Engage in fierce battles, defy
+          overwhelming odds, and emerge triumphant against all odds. The moment
+          to strike at Infernus is at hand. Remember, brave adventurer, your
+          destiny lies within your grasp. Embrace the challenge, for the
+          ultimate victory over Infernus, Lord of Oblivion, awaits the fearless
+          and determined.
         </p>
+        <button className='bg-gradient-to-br px-7 py-4 from-amber-800 to-stone-700 text-white font-semibold font-play rounded flex gap-1 items-center shadow-md shadow-neutral-500 active:translate-y-2 transition-all duration-150 ease-out'>
+          <p className='text-2xl'>PLAY</p>
+          <FontAwesomeIcon icon={faGamepad} className='text-3xl' />
+        </button>
       </article>
     </Container>
   )
