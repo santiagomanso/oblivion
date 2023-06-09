ae7e726439302253d8be07522e6e09bdb5a77104
santiagomanso <santiagomanso@web.de>
2023-06-09 19:16:00 +0200

    💫 added fadeIn animation

--- src/components/containers/Container.tsx
+++ src/components/containers/Container.tsx
@@ -3,7 +3,7 @@ import { ContainerI } from '@/interfaces/containerInterface'
 const Container = ({ children, flex, col, colCenter, style }: ContainerI) => {
   return (
     <section
-      className={`bg-gray-50 rounded-md shadow-md w-full max-w-6xl h-3/4
+      className={`bg-gray-50 rounded-md shadow-md w-full max-w-6xl h-3/4 animate__animated animate__fadeIn
               ${style ? style : ''}
                 `}
     >
