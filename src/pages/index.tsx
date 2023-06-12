82c365ddbbbb4c8c8170e57958d543612b84ff0c
santiagomanso <santiagomanso@web.de>
2023-06-12 11:21:58 +0200

    🔧 added next Image component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import Container from '@/components/containers/Container'
 import { faGamepad } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <Container flex style='flex'>
-      <article className='flex items-center justify-center w-3/4 '>
-        <img
+      <article className='flex items-center justify-center w-3/4 relative'>
+        <Image
           src='https://i.pinimg.com/originals/b7/9f/9b/b79f9b27f5bd33fae89f352b6dfb06a5.gif'
           alt=''
-          className='w-full h-full object-cover'
+          fill={true}
+          className='w-full h-3/4 rounded object-cover'
         />
       </article>
       <article className='flex flex-col gap-3 px-10 items-center justify-center w-1/2'>
@@ -32,10 +35,13 @@ export default function Home() {
           ultimate victory over Infernus, Lord of Oblivion, awaits the fearless
           and determined.
         </p>
-        <button className='bg-gradient-to-br px-7 py-4 from-amber-800 to-stone-700 text-white font-semibold font-play rounded flex gap-1 items-center shadow-md shadow-neutral-500 active:translate-y-2 transition-all duration-150 ease-out'>
+        <Link
+          href='/selectPlayer'
+          className='bg-gradient-to-br px-7 py-4 from-amber-800 to-stone-700 text-white font-semibold font-play rounded flex gap-1 items-center shadow-md shadow-neutral-500 active:translate-y-2 transition-all duration-150 ease-out'
+        >
           <p className='text-2xl'>PLAY</p>
           <FontAwesomeIcon icon={faGamepad} className='text-3xl' />
-        </button>
+        </Link>
       </article>
     </Container>
   )
