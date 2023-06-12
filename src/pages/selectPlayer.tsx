2934c318294524957a67507fa6c5bc7362f89a67
santiagomanso <santiagomanso@web.de>
2023-06-12 16:27:54 +0200

    🔥 cards / select player ready!

--- src/pages/selectPlayer.tsx
+++ src/pages/selectPlayer.tsx
@@ -1,7 +1,24 @@
+import Card from '@/components/Card'
 import Container from '@/components/containers/Container'
+import { playerContext } from '@/context/playerContext'
+import { playersData } from '@/data/players/playersData'
+import { useContext } from 'react'
 
-const selectPlayer = () => {
-  return <Container>selectPlayer</Container>
+const SelectPlayer = () => {
+  const { setPlayer } = useContext(playerContext)
+
+  const bgImg = 'https://i.ibb.co/f1RMfVP/char-Selection-BG.png'
+  return (
+    <Container style='grid grid-cols-2 place-items-center' bgImg={bgImg}>
+      {playersData ? (
+        playersData.map((char) => (
+          <Card player={char} key={char.id} setPlayer={setPlayer} />
+        ))
+      ) : (
+        <span className='animate-ping'>loading</span>
+      )}
+    </Container>
+  )
 }
 
-export default selectPlayer
+export default SelectPlayer
