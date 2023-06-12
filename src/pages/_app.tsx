4716f9a7bfd0171062156bd8923dfa81118c8786
santiagomanso <santiagomanso@web.de>
2023-06-12 16:27:30 +0200

    🔥 added context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,19 @@ import Layout from '@/components/containers/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 //imports for fontawesome icons
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { PlayerProvider } from '@/context/playerContext'
 
-library.add(fas);
+library.add(fas)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PlayerProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlayerProvider>
   )
 }
