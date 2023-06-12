b4f37ea7656b007eded98c56c980cf407ae2c20d
santiagomanso <santiagomanso@web.de>
2023-06-12 16:24:42 +0200

    🚧 🐛 next image keeps super beeing super large

--- src/components/languageSwitcher.tsx
+++ src/components/languageSwitcher.tsx
@@ -1,14 +1,7 @@
-import Image from 'next/image'
-
 const languageSwitcher = () => {
   return (
     <div className='flex gap-1 items-center'>
-      <Image
-        alt='flag'
-        fill={true}
-        src='https://i.ibb.co/vVRn7jn/en.png'
-        className='w-8'
-      />
+      <img src='https://i.ibb.co/vVRn7jn/en.png' className='w-8' />
       <span>en</span>
     </div>
   )
